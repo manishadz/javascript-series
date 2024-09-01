31d0b1b056ff885d51cea644e9020f7f6760f7ec
Manish adz <71862308+manishadz@users.noreply.github.com>
2024-09-01 16:57:41 +0000

    made variable

---
+++ Javascript/varible.js
@@ -0,0 +1,9 @@
+const accountId = 234
+let accountEmail = "mundredon1@gmail.com"
+var accountPassword ="Apple@123"
+accountCity ="Dharan"
+
+// accountId =123
+
+// console.log(accountId)
+console.table(accountId)
